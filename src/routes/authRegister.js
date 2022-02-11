8281dce757f8f2cd91686775c9fd78b1a2671ea6
luong ngoc <ngocthien250800@gmail.com>
2022-02-11 16:47:12 +0700

    login_register

--- src/routes/authRegister.js
+++ src/routes/authRegister.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const AuthRegisterController = require('../app/controllers/AuthRegisterController');
 
-router.get('/', AuthRegisterController.register);
 router.get('/auth/user', AuthRegisterController.authUserRegister);
+router.post('/auth/user', AuthRegisterController.handlingRegister);
+router.get('/', AuthRegisterController.register);
 
 module.exports = router;
